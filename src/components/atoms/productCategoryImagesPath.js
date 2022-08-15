b1a2fda0a24c9f8327cba3422991775f7fa6a950
adrsimon <adrisim2001@gmail.com>
2022-08-15 10:16:40 +0200

    created the product categories and rendered them

---
+++ src/components/atoms/productCategoryImagesPath.js
@@ -0,0 +1,13 @@
+export const ProductImages = {
+    tapBeer: require('../../../assets/productImages/tapBeer.png'),
+    fruits: require('../../../assets/productImages/fruits.png'),
+    bottleBeer: require('../../../assets/productImages/bottleBeer.png'),
+    softs: require('../../../assets/productImages/softs.png'),
+    petitDej: require('../../../assets/productImages/petitDej.png'),
+    repas: require('../../../assets/productImages/repas.png'),
+    vrac: require('../../../assets/productImages/vrac.png'),
+    pampryls: require('../../../assets/productImages/pampryls.png'),
+    default: require('../../../assets/productImages/bottleBeer.png'),
+};
+
+export default ProductImages;
