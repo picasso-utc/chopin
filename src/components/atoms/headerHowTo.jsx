43f16cc4edc6910d9c03d39358aa1aef06f51000
geo saglio <geosaglioo@gmail.com>
2022-08-16 16:10:40 +0200

    Vie-Du-Pic Page without the Event Section

---
+++ src/components/atoms/headerHowTo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const HeaderHowTo = () => {
+    return (
+        <Text
+            style={{
+                fontSize: 20,
+                marginTop: 20,
+                color: '#FFFFFF',
+            }}
+        >
+            DÉMARCHES COMMUNES
+        </Text>
+    );
+};
+
+export default HeaderHowTo;
