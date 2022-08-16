3fce30704cc2b77ec9a55885e9b626956661cff8
adrsimon <adrisim2001@gmail.com>
2022-08-16 18:41:31 +0200

    fixed display of the vie du pic elements

--- src/components/screens/viedupicScreen.jsx
+++ src/components/screens/viedupicScreen.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import DivLinkVideo from '../molecules/divLinkVideo';
+import HowTo from '../molecules/howTo';
+
 function ViedupicScreen() {
     return (
         <View
             style={{
                 backgroundColor: '#000223',
                 height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
             }}
-        />
+        >
+            <DivLinkVideo />
+            <HowTo />
+        </View>
     );
 }
 
