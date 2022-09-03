3ad93905dc05eb9debe111a16bf75c730971127f
geo saglio <geosaglioo@gmail.com>
2022-09-03 15:26:06 +0200

    Calendar now gets info from a mock

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -7,14 +7,16 @@ const mock = new MockAdapter(axios);
 
 if (config.isMock) {
     mock.onGet(`${config.API_BASE_URL}/newsletter`).reply(200, config.mockNeswletter);
-    mock.onGet(`${config.API_BASE_URL}/calendar`).reply(200, config.mockEvenements);
+    mock.onGet(`${config.API_BASE_URL}/events`).reply(200, config.mockEvenements);
     mock.onGet(`${config.API_BASE_URL}/payutc/public/articles`).reply(200, config.mockCarte);
     mock.onGet(`${config.API_BASE_URL}/trending_product`).reply(200, config.mockTrending);
+    mock.onGet(`${config.API_BASE_URL}/calendar`).reply(200, config.mockCalendrier);
 } else {
     mock.onAny().passThrough();
 }
 
 export const getNewsletter = () => axios.get(`${config.API_BASE_URL}/newsletter`);
 export const getCarte = () => axios.get(`${config.API_BASE_URL}/payutc/public/articles`);
-export const getEvenements = () => axios.get(`${config.API_BASE_URL}/calendar`);
+export const getEvenements = () => axios.get(`${config.API_BASE_URL}/events`);
 export const getTrendingProduct = () => axios.get(`${config.API_BASE_URL}/trending_product`);
+export const getCalendar = () => axios.get(`${config.API_BASE_URL}/calendar`);
--- src/components/screens/calendarScreen.jsx
+++ src/components/screens/calendarScreen.jsx
@@ -1,76 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, ScrollView } from 'react-native';
 
 import DayCalendar from '../molecules/dayCalendar';
+import { getCalendar } from '../../api/apiCalls';
 import ScreenStyle from '../style/screenStyle';
 
 function CalendarScreen() {
+    const [calendar, setCalendar] = useState({});
+
+    useEffect(() => {
+        getCalendar()
+            .then((res) => {
+                setCalendar(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .done();
+    }, []);
+
+    const date = new Date().toISOString();
+    let today = date.split('T')[0];
+
+    const week = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+
     return (
         <View style={ScreenStyle.calendarScreenContainer}>
-            <DayCalendar
-                id={1}
-                today
-                weekday="lundi"
-                day="21"
-                morning="FEVITE"
-                noon="FEVITE"
-                night="FEVITE"
-            />
             <ScrollView contentContainerStyle={ScreenStyle.ScreenScrollContainer} scrollEnabled>
-                <DayCalendar
-                    id={2}
-                    today={false}
-                    weekday="mardi"
-                    day="21"
-                    morning="Pic en Transition"
-                    noon="Formul'UT"
-                    night="Perm Chefs"
-                />
-                <DayCalendar
-                    id={3}
-                    today={false}
-                    weekday="mercredi"
-                    day="21"
-                    morning="Ackrockeur"
-                    noon="Integ"
-                    night="Perms des Profs"
-                />
-                <DayCalendar
-                    id={4}
-                    today={false}
-                    weekday="jeudi"
-                    day="21"
-                    morning="Rugby"
-                    noon="Rugby"
-                    night="Rugby"
-                />
-                <DayCalendar
-                    id={5}
-                    today={false}
-                    weekday="vendredi"
-                    day="21"
-                    morning="FEVITE"
-                    noon="FEVITE"
-                    night="FEVITE"
-                />
-                <DayCalendar
-                    id={6}
-                    today={false}
-                    weekday="samedi"
-                    day="21"
-                    morning="FEVITE"
-                    noon="FEVITE"
-                    night="FEVITE"
-                />
-                <DayCalendar
-                    id={7}
-                    today={false}
-                    weekday="dimanche"
-                    day="21"
-                    morning="FEVITE"
-                    noon="FEVITE"
-                    night="FEVITE"
-                />
+                {Object.entries(calendar).map(([key, value], index) => (
+                    <DayCalendar
+                        key={index}
+                        id={index}
+                        today={key === today}
+                        weekday={week[new Date(key).getDay()]}
+                        day={new Date(key).getDate()}
+                        perms={value}
+                    />
+                ))}
             </ScrollView>
         </View>
     );
